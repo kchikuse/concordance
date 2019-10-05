e93d5bfb0d8995536c7adddb315d076ad4d5a26f
K Chikuse <kchikuse@gmail.com>
2019-10-05 23:00:37 +0200

    improvements

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 addEventListener("DOMContentLoaded", () => {
     const $ = e => document.querySelector(e);
     const analysis = $(".analysis");
+    const details = $("details");
 
     document.onclick = async event => {
-        const e = event.target;
+        let e = event.target;
+        const tag = e.tagName.toLowerCase();
 
-        if (e.tagName.toLowerCase() == "w") {
-            const hasLemma = e.hasAttribute("lemma");
-            const sn = hasLemma ? e.getAttribute("lemma").split(":").pop() :
-                e.getAttribute("src");
-
-            if (!sn) return;
-
-            const response = await fetch(`sn/${sn}`);
-            analysis.innerHTML = await response.text();
+        if(["w", "divinename"].indexOf(tag) < 0) {
+            return;
         }
+
+        if (tag == "divinename") {
+            e = e.closest("w");
+        }
+
+        const sn = e.hasAttribute("lemma") ?
+            e.getAttribute("lemma").split(":").pop() :
+            e.getAttribute("src");
+
+        if (!sn) return;
+
+        const response = await fetch(`sn/${sn}`);
+        analysis.innerHTML = await response.text();
+        details.removeAttribute("open");
     };
 
     (() => {
